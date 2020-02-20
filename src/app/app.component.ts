a20186a11a15ebe6daf678a742740071911321e3
DanBaisal <dan.baisal92@gmail.com>
2020-02-20 21:42:22 +0100

    Added some styling and several pipes, including custom ones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string;
+  date: string;
+  amount: number;
+  height: number;
+  miles: number;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000
+  }
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
+
+  onHeightChange(value: string) { // Decimal Pipe
+    this.height = parseFloat(value);
+  }
 
   onNameChange(value: string) {
     this.name = value;
   }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
 }
+
+// Value received on an event is always received as a string even though it may be a number
